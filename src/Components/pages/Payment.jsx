9990f62b17030c623f7a6154257fa2dfb704f9f2
PraveenchandraR <118369839+PraveenchandraR@users.noreply.github.com>
2023-07-31 10:45:15 +0530

    Add files via upload

---
+++ src/Components/pages/Payment.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import '../Styles/payment.css';
+
+
+function Payment() {
+    return (
+        <>
+            {/* <ProfileRow />
+        <Navigation /> */}
+        <div className="payment">
+            
+            <div> <h1>Order</h1><br />
+                <h3>Only Pay After Receive</h3><br />
+            </div>
+                <div className="paybox">
+                      <div className="paytype">
+                <span className="payname">Order Id: 57CUD877CXUE6</span>
+                <span className="payname">Thankyou for shopping !!!</span>
+            </div>
+           </div>
+        
+            </div>
+            </>
+    )
+}
+export default Payment;
